6f4e273fce2a20e3aef205b61ea96ab02bbe0499
Westley Feller <feller.westley@gmail.com>
2019-08-08 12:15:45 -0700

    Refactored challenge 4 to use only reduce

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -474,7 +474,7 @@ console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-const ticketPriceTotal = runners.map(runner => runner.donation).reduce((previousValue, currentValue) => previousValue + currentValue);
+const ticketPriceTotal = runners.reduce((accumulator, runner) => accumulator + runner.donation, 0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
