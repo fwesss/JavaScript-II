8c2ae46f90b6d9a61f1c956987292ac97a43ac5b
Westley Feller <feller.westley@gmail.com>
2019-08-08 19:35:18 -0700

    Reformatted files

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,11 +1,13 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a higher order function and invoke the callback function to test your work. You have been
+// provided an example of a problem and a solution to see how this works with our items array.
+// Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
+/*
 
-  //Given this problem: 
-  
+  //Given this problem:
+
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
   }
@@ -17,7 +19,7 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
     return cb(arr[0]);
   }
 
-  // Function invocation 
+  // Function invocation
   firstItem(items, function(first) {
     console.log(first)
   });
@@ -26,61 +28,66 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 
 function getLength(arr, cb) {
-    // getLength passes the length of the array into the callback.
-    return cb(arr.length);
+  // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
-getLength(items, length => {
-    console.log(length);
+getLength(items, (length) => {
+  console.log(length);
 });
 
-function last(arr, cb) {
-    // last passes the last item of the array into the callback.
-    return cb(arr[arr.length - 1]);
+
+function getLast(arr, cb) {
+  // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
 
-last(items, last => {
-    console.log(last);
+getLast(items, (last) => {
+  console.log(last);
 });
+
 
 function sumNums(x, y, cb) {
-    // sumNums adds two numbers (x, y) and passes the result to the callback.
-    return cb(x + y);
+  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
 
-sumNums(3, 5, sum => {
-    console.log(sum);
+sumNums(3, 5, (sum) => {
+  console.log(sum);
 });
+
 
 function multiplyNums(x, y, cb) {
-    // multiplyNums multiplies two numbers and passes the result to the callback.
-    return cb(x * y);
+  // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
 
-multiplyNums(5, 7, multiply => {
-    console.log(multiply);
+multiplyNums(5, 7, (multiply) => {
+  console.log(multiply);
 });
 
-function contains(item, list, cb) {
-    // contains checks if an item is present inside of the given array/list.
-    // Pass true to the callback if it is, otherwise pass false.
-    return cb(list.includes(item));
+
+function findDuplicates(item, list, cb) {
+  // contains checks if an item is present inside of the given array/list.
+  // Pass true to the callback if it is, otherwise pass false.
+  return cb(list.includes(item));
 }
 
-contains("Notebook", items, contains => {
-    console.log(contains);
+findDuplicates('Notebook', items, (contains) => {
+  console.log(contains);
 });
+
 
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
-    // removeDuplicates removes all duplicate values from the given array.
-    // Pass the duplicate free array to the callback function.
-    // Do not mutate the original array.
+  // removeDuplicates removes all duplicate values from the given array.
+  // Pass the duplicate free array to the callback function.
+  // Do not mutate the original array.
 
-    return cb(Array.from(new Set(array)));
+  return cb(Array.from(new Set(array)));
 }
 
-removeDuplicates(items, clean => {
-    console.log(clean);
+removeDuplicates(items, (clean) => {
+  console.log(clean);
 });
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,17 +1,20 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-const name = "Westley Feller";
+const name = 'Westley Feller';
 const displayName = () => name;
 
 console.log(displayName());
 
-/* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
+/*
+STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project
+files
+*/
 
 
 // ==== Challenge 2: Create a counter function ====
 const count = () => {
-    let count = 0;
-    return () => ++count;
+  let count = 0;
+  return () => ++count;
 };
 
 const newCounter = count();
@@ -25,18 +28,14 @@ console.log(newCounter());
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
-    // Return an object that has two methods called `increment` and `decrement`.
-    // `increment` should increment a counter variable in closure scope and return it.
-    // `decrement` should decrement the counter variable and return it.
-    let count = 0;
-    return {
-        increment: () => {
-            return ++count;
-        },
-        decrement: () => {
-            return --count;
-        }
-    }
+  // Return an object that has two methods called `increment` and `decrement`.
+  // `increment` should increment a counter variable in closure scope and return it.
+  // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+  return {
+    increment: () => ++count,
+    decrement: () => --count,
+  };
 };
 
 console.log(counterFactory().decrement());
